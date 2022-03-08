7a58601ca4c4be6b9e8619443c2f57795d1b8fa2
Samuel Banning <samcbanning@gmail.com>
2022-03-08 16:31:58 -0500

    fixed

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-
-
 setup(
     name="quantum_torch_ops",
+    ext_modules=[
+        CppExtension(
+            "quantum_ops",
+            ["quantum_torch_ops/circuit.cpp"],
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension.with_options(no_python_abi_suffix=True)},
 )
